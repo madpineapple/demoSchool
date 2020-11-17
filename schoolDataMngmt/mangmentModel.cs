f756c7be44427513c01b3425f2e13c8a5f29a830
Charles Daoust <charles.daoust135@gmail.com>
2020-11-17 04:41:46 -0500

    working on data access

---
+++ schoolDataMngmt/mangmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolDataMngmt
+{
+    class mangmentModel
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+    }
+}
---
+++ schoolDataMngmt/teacherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolDataMngmt
+{
+    class teacherModel
+    {
+
+        public int TeacherId { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string expertise { get; set; }
+        public int age { get; set; }
+        public int dateOfHire { get; set; }
+    }
+}
