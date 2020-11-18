8d011a88ffa688b240359dddf5efeb0d103459e9
Charles Daoust <charles.daoust135@gmail.com>
2020-11-18 05:08:41 -0500

    Added Identity scaffolding

---
+++ schoolManagerWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using schoolManagerWebsite.Data;
+
+[assembly: HostingStartup(typeof(schoolManagerWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace schoolManagerWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<schoolManagerWebsiteContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("schoolManagerDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<schoolManagerWebsiteContext>();
+            });
+        }
+    }
+}
